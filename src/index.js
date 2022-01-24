623386492517231465d83016ab8f0ceb7ac9a60b
Daniel Berg <Berg.iodaniel@gmail.com>
2022-01-24 21:00:16 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './styles.css';
 import { startUp } from "./modules/viewController";
-import wood from "./images/wood.jpg";
 
 startUp(); // TODO: Create this
-
-// const myImg = new Image();
-// myImg.src = wood;
-// document.querySelector('body').style.backgroundImage = myImg;
